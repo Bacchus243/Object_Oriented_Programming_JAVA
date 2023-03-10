3d176b29bd87fbf73287460fa2fb6a57c4084e32
Trenton Meryhew <trentonmeryhew@trentons-mbp.lan>
2023-03-10 16:45:43 -0700

    Created Week 6 Folder

---
+++ Week 6/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+   public static void main(String[] args) {
+      final int NUM_ELEMENTS = 6;
+      Scanner scnr = new Scanner(System.in);
+      ArrayList<Integer> listInts = new ArrayList<Integer>();
+      // Hint: Declare listNegInts
+      int i;
+      
+      // Get input integers
+      for (i = 0; i < NUM_ELEMENTS; ++i) {
+         listInts.add(scnr.nextInt());
+      }
+      
+      /* Type your code here. */
+   }
+}
