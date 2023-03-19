d4c05f0fe85af907cbd77c9681a950a13bf27b8a
Trenton Meryhew <trenton@trentons-imac.lan>
2023-03-18 20:47:34 -0600

    Added Lab 7.20 - Sorted Numbers Linked List

---
+++ Lesson 7 - Linked Lists/SortedNumberList.java
@@ -0,0 +1,27 @@
+
+public class SortedNumberList {
+	public Node head;
+	public Node tail;
+
+	public SortedNumberList() {
+		head = null;
+		tail = null;
+	}
+
+   // Optional: Add any desired private methods here
+   
+   
+	// Inserts the number into the list in the correct position such that the
+	// list remains sorted in ascending order.
+	public void insert(double number) {
+	   // Your code here
+	}
+
+	// Removes the node with the specified number value from the list. Returns
+	// true if the node is found and removed, false otherwise.
+	public boolean remove(double number) {
+	   // Your code here
+	   
+	   return false;
+	}
+}
