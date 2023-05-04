834d3b537368a03b8c98e2a1061654122d7a8844
Trenton Meryhew <trenton@trentons-imac.lan>
2023-05-03 22:28:01 -0600

    Finished Final Project

--- Meryhew - Final Project/src/Sentiment.java
+++ Meryhew - Final Project/src/Sentiment.java
@@ -1,34 +1,48 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Sentiment {
 
 	@SuppressWarnings("resource")
 	public static void start (Scanner scnr) {
-
-		System.out.println("\n");
-		System.out.println("Sentiment Analysis.\n");
 		
 		HashSet<String> posHash = new HashSet<String>();
 		FileInputStream positiveInStream = null;
+		
 		HashSet<String> negHash = new HashSet<String>();
 		FileInputStream  negativeInStream = null;
+		
 		FileInputStream userFileStream = null;
 		
 		String currLine;
 		String currWord;
+		String userFileName = null;
+		String currPosWord = "";
+		String currNegWord = "";
+		String sentiment = null;
+		String doAgain = "Y";
+		
+		int posWordsLoaded = 0;
+		int negWordsLoaded = 0;		
+		int totalUserWords = 0;
 		int numPosWords = 0;
 		int numNegWords = 0;
-		String userFileName = null;
 		
-		boolean posWord = false;
-		boolean negWord = false;
+		double numberPosWords;
+		double numberNegWords;
+		double percentPos = 0;
+		double percentNeg = 0;
 		
+		Iterator<String> posIT;
+		Iterator<String> negIT;
 		
-		
-		
+
+		System.out.println("\n");
+		System.out.println("Sentiment Analysis.\n");
+
 		// Read in positive words to posHash
 		try {
 			positiveInStream = new FileInputStream("positive.txt");
@@ -45,7 +59,7 @@ public class Sentiment {
 			try {
 				if (!(currLine.charAt(0)== ';')) {
 					posHash.add(currLine.toUpperCase());
-					++numPosWords;
+					++posWordsLoaded;
 				}	
 			}
 			catch (Exception e) {
@@ -70,7 +84,7 @@ public class Sentiment {
 			try {
 				if (!(currLine.charAt(0)== ';')) {
 					negHash.add(currLine.toUpperCase());
-					++numNegWords;
+					++negWordsLoaded;
 				}
 			}
 			catch (Exception e) {
@@ -79,66 +93,102 @@ public class Sentiment {
 		
 		
 		
-		System.out.println(numPosWords + " Positive Words Successfully Loaded");
-		System.out.println(numNegWords + " Negative Words Successfully Loaded");
+		System.out.println(posWordsLoaded + " Positive Words Successfully Loaded");
+		System.out.println(negWordsLoaded + " Negative Words Successfully Loaded");
 		
 		
 		
 		
-		// Ask user for name of file
-		System.out.print("\nEnter the name of the text file to perform sentiment analysis:  ");
-		userFileName = scnr.next();
 		
+		while (doAgain.equals("Y")) {
+			
 		
+			// Ask user for name of file
+			System.out.print("\nEnter the name of the text file to perform sentiment analysis:  ");
+			userFileName = scnr.next();
+			
+			
+			
+			// Open filestream for user's file
+			try {
+				userFileStream = new FileInputStream(userFileName);
+			} catch (FileNotFoundException e) {
+				System.out.println("File Not Found!");
+			}
+			
+			
+			
+			// Analyze each word in User's file
+			inFS = new Scanner(userFileStream);
+			while (inFS.hasNext()) {
+				
+				// Read in next word
+				currWord = inFS.next();
+				currWord = currWord.toUpperCase();
+				currWord = currWord.replaceAll("[^a-zA-Z]","");
+				++totalUserWords;
+				
 		
-		
-		// Open filestream for user's file
-		try {
-			userFileStream = new FileInputStream(userFileName);
-		} catch (FileNotFoundException e) {
-			System.out.println("File Not Found!");
+				// Compare each word to Positive HashSet
+					// if word == positive, set PosWord == TRUE && increment positive counter
+				posIT = posHash.iterator();
+				while (posIT.hasNext()) {
+					currPosWord = posIT.next();
+					if (currWord.equals(currPosWord)) {
+						++numPosWords;
+					}
+				}
+				
+				
+				// IF PosWord == FALSE, Compare each word to Negative HashSet
+					// if word == negative, set NegWord == TRUE && increment negative counter
+				negIT = negHash.iterator();
+				while (negIT.hasNext()) {
+					currNegWord = negIT.next();
+					if (currWord.equals(currNegWord)) {
+						++numNegWords;
+					}
+				}
+			}
+			
+		// Math for running the calculations on the results
+			numberPosWords = numPosWords;
+			numberNegWords = numNegWords;
+			
+			percentPos = (numberPosWords / totalUserWords) * 100;
+			percentNeg = (numberNegWords / totalUserWords) * 100;
+			
+			
+			if ((percentPos - percentNeg) >= 5) {
+				sentiment = "positive";
+			}
+			
+			if ((percentNeg - percentPos) >= 5) {
+				sentiment = "negative";
+			}
+			
+			
+			
+			System.out.println("\nSentiment Report for " + userFileName + ":");
+			
+			System.out.println("There were " + numPosWords + " positive words, " 
+								+ numNegWords + " negative words and " + totalUserWords + " total words.");
+			
+			System.out.printf("That's %.1f%% positive and %.1f%% negative. Overall the file's sentiment was %s.\n\n",  
+									percentPos, percentNeg, sentiment);
+			
+			
+			System.out.print("Would you like to analyze another file Y/N? ");
+			doAgain = scnr.next();
+			
+		// Zero out variables
+			numPosWords = 0;
+			numNegWords = 0;
 		}
-		
-		
-		
-		
-		// Analyze each word in User's file
-		inFS = new Scanner(userFileStream);
-		while (inFS.hasNext()) {
 			
-			// Read in next word
-			currWord = inFS.next();
-			currWord = currWord.toUpperCase();
-			System.out.println(currWord);
-	
-			
-			// Compare each word to Positive HashSet
-				
-				// if word == positive, set PosWord == TRUE && increment positive counter
-				
-				
-				
-			// IF PosWord == FALSE, Compare each word to Negative HashSet
-				
-				// if word == negative, set NegWord == TRUE && increment negative counter
-				
-				
-				
-			// if neutral, increment neutral counter
-
-	
-			
-			// Reset PosWord && NegWord to == FALSE
-		
-		}
-		
-		
-	// Math method for running the calculations on the results 
-		
 		
 	// Print Method for outputting the information 
 		
-		inFS.close();
 	}
 }
 
